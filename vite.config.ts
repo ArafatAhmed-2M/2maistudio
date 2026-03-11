2690bcf22db13afb7c83ed158f3d45679b8ea1d0
Arafat Ahmed Mubin <futureofmyebooks@gmail.com>
2026-03-11 12:45:14 +0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/2maistudio/', // This MUST match your repository name
+})
 import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
